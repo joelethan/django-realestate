e5ec19f284e86d6812fb14ac36a236b303985701
Katusiime Joel Ian <joelethan@users.noreply.github.com>
2019-09-05 03:41:41 +0300

    Add search through listings functionality

--- listings/views.py
+++ listings/views.py
@@ -25,9 +25,52 @@ def listing(request, listing_id):
 
 
 def search(request):
+    quesryset_list = Listing.objects.order_by('-list_date')
+
+    # Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            quesryset_list = quesryset_list.filter(
+                discription__icontains=keywords)
+
+    # City
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            quesryset_list = quesryset_list.filter(
+                city__iexact=city)
+
+    # State
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            quesryset_list = quesryset_list.filter(
+                state__iexact=state)
+
+    # Bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            quesryset_list = quesryset_list.filter(
+                bedrooms__lte=bedrooms)
+
+    # Price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            quesryset_list = quesryset_list.filter(
+                price__lte=price)
+
+    paginator = Paginator(quesryset_list, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
     context = {
         'state_choices': state_choices,
         'bedroom_choices': bedroom_choices,
         'price_choices': price_choices,
+        'listings': paged_listings,
+        'values': request.GET
     }
     return render(request, 'listings/search.html', context)
